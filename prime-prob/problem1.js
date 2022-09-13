1ef0e1877598cf8182dd7f2d4cb5d5700592350c
vikas jadaun <deadpoolji125@gmail.com>
2022-09-13 12:14:11 +0530

    prime-palin

---
+++ prime-prob/problem1.js
@@ -0,0 +1,15 @@
+// check palindrome number
+let str="vikas";
+
+let bag="";
+
+for(i=str.length-1;i>=0;i--){
+    bag=bag+str[i];
+}
+
+if(bag========str){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
---
+++ prime-prob/problem2.js
@@ -0,0 +1,14 @@
+// check prime number
+let num=13;
+let count=0;
+  if(num%num==0 && num%1==0){
+      count++;
+     
+  }
+     if(count==2){
+             console.log("Yes");
+    }
+     else{
+             console.log("No");
+    }
+
